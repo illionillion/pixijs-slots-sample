b109d265296ae558d3c3273d58a85e397bd68b27
yuto ichinose <yuto.0526.sano@gmail.com>
2023-04-21 22:23:31 +0900

    キャラを表示して動かせる様にはなったけど、なぜか方向転換ができない、むり

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Application, Graphics, Text } from "pixi.js";
+import {
+  AnimatedSprite,
+  Application,
+  Assets,
+  Container,
+  Spritesheet,
+} from "pixi.js";
 
 /**
  * ウィンドウ横幅初期値
@@ -9,7 +15,7 @@ const initialWidth = 400;
  */
 const initialHeight = 200;
 
-const main = () => {
+const main = async () => {
   /**
    * Applicationオブジェクト生成
    */
@@ -28,35 +34,103 @@ const main = () => {
   onResize(); // 初回リサイズ
   window.addEventListener("resize", onResize); // 登録
 
-  // const loader = PIXI.loader.shared; // 使えない
+  const playerTexture = await Assets.load("./assets/image/player/player.png"); // これでシートJSONの読み込みはできる
+  console.log(playerTexture);
+  const playerSprite = await Assets.load(
+    "./assets/image/player/player.sprite.json"
+  );
+  const playerObject = new Spritesheet(playerTexture, playerSprite.data); // playerSprite.data でなんかいけた
+  console.log(await playerObject.parse());
+  const frontTextures = [
+    playerObject.textures["front-1.png"],
+    playerObject.textures["front-2.png"],
+  ];
+  const frontAnimation = new AnimatedSprite(frontTextures);
+  frontAnimation.animationSpeed = 0.1;
+  frontAnimation.play();
+  frontAnimation.scale.x = 3;
+  frontAnimation.scale.y = 3;
 
-  const text = new Text("Hello World!");
-  text.style.fill = "#ffffff";
-  text.anchor.set(0.5);
-  text.x = app.screen.width / 2;
-  text.y = app.screen.height / 2;
+  const backTextures = [
+    playerObject.textures["back-1.png"],
+    playerObject.textures["back-2.png"],
+  ];
+  const backAnimation = new AnimatedSprite(backTextures);
+  backAnimation.animationSpeed = 0.1;
+  backAnimation.play();
+  backAnimation.scale.x = 3;
+  backAnimation.scale.y = 3;
 
-  const graphics = new Graphics();
-  graphics.beginFill(0xff0000);
-  graphics.drawRect(0, 0, 100, 100);
-  graphics.endFill();
-  graphics.x = 0;
-  graphics.y = 0;
-  app.stage.addChild(graphics);
-  app.stage.addChild(text);
-  // ゲームループを開始する
-  app.ticker.add(() => {
-    graphics.x += 1;
-    graphics.y += 1;
-    if (graphics.x > app.screen.width) {
-      graphics.x = 0;
+  const leftTextures = [
+    playerObject.textures["left-1.png"],
+    playerObject.textures["left-2.png"],
+  ];
+  const leftAnimation = new AnimatedSprite(leftTextures);
+  leftAnimation.animationSpeed = 0.1;
+  leftAnimation.play();
+  leftAnimation.scale.x = 3;
+  leftAnimation.scale.y = 3;
+
+  const rightTextures = [
+    playerObject.textures["right-1.png"],
+    playerObject.textures["right-2.png"],
+  ];
+  const rightAnimation = new AnimatedSprite(rightTextures);
+  rightAnimation.animationSpeed = 0.1;
+  rightAnimation.play();
+  rightAnimation.scale.x = 3;
+  rightAnimation.scale.y = 3;
+
+  const playerAnimatedSprite = new AnimatedSprite(frontTextures);
+  playerAnimatedSprite.animationSpeed = 0.1;
+  playerAnimatedSprite.play();
+
+  // キャラクターの初期方向を前向きに設定する
+  let currentAnimation = frontAnimation;
+
+  // キャラクターを画面中央に配置する
+  const character = new Container();
+  character.addChild(
+    frontAnimation,
+    leftAnimation,
+    rightAnimation,
+    backAnimation
+  );
+  character.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  // キー入力に応じてキャラクターを動かす
+  document.addEventListener("keydown", (e) => {
+    const keyCode = e.code;
+    switch (keyCode) {
+      case "ArrowUp":
+        e.preventDefault();
+        currentAnimation = backAnimation;
+        character.y -= 10;
+        break;
+      case "ArrowDown":
+        e.preventDefault();
+        currentAnimation = frontAnimation;
+        character.y += 10;
+        break;
+      case "ArrowLeft":
+        e.preventDefault();
+        currentAnimation = leftAnimation;
+        character.x -= 10;
+        break;
+      case "ArrowRight":
+        e.preventDefault();
+        currentAnimation = rightAnimation;
+        character.x += 10;
+        break;
     }
-    if (graphics.y > app.screen.height) {
-      graphics.y = 0;
-    }
-    text.x = app.screen.width / 2;
-    text.y = app.screen.height / 2;
+    currentAnimation.play();
   });
+
+  // ゲームループを開始する
+  app.ticker.add(() => {});
+
+  // キャラクターを描画領域に追加
+  app.stage.addChild(character);
 };
 
 window.addEventListener("load", main);
